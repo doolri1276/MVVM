b04770004789120e01247cf57d3a2e435e2e02a9
doolri1276 <doolri1277@gmail.com>
2018-09-18 11:12:52 +0900

    완료

---
+++ app/src/main/java/com/snownaul/mvvm/adapters/UserListViewAdapter.java
@@ -0,0 +1,57 @@
+package com.snownaul.mvvm.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.snownaul.mvvm.R;
+import com.snownaul.mvvm.databinding.ListItemUserBinding;
+import com.snownaul.mvvm.model.UserViewModel;
+import com.snownaul.mvvm.tools.App;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ObservableArrayList;
+
+public class UserListViewAdapter extends BaseAdapter {
+
+    private ObservableArrayList<UserViewModel> users = new ObservableArrayList<>();
+
+    public void addAll(ObservableArrayList<UserViewModel> users){
+        this.users=users;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return users.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return users.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ListItemUserBinding binding;
+
+        if(convertView==null){
+            LayoutInflater inflater = LayoutInflater.from(App.getInstance());
+            binding = DataBindingUtil.inflate(inflater, R.layout.list_item_user, parent, false);
+            convertView = binding.getRoot();
+            convertView.setTag(binding);
+        }else{
+            binding = (ListItemUserBinding) convertView.getTag();
+        }
+
+        binding.setUser(users.get(position));
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/snownaul/mvvm/view/ListActivity.java
+++ app/src/main/java/com/snownaul/mvvm/view/ListActivity.java
@@ -1,16 +1,44 @@
 package com.snownaul.mvvm.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ObservableArrayList;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.snownaul.mvvm.R;
+import com.snownaul.mvvm.adapters.UserListViewAdapter;
+import com.snownaul.mvvm.databinding.ActivityListBinding;
+import com.snownaul.mvvm.model.UserViewModel;
+import com.snownaul.mvvm.view_model.UsersViewModel;
 
 public class ListActivity extends AppCompatActivity {
+
+    ActivityListBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_list);
+        binding.setViewModel(new UsersViewModel());
+
+        getSupportActionBar().setTitle("List Activity");
+
+    }
+
+    @BindingAdapter("app:items")
+    public static void setUserList(ListView listView, ObservableArrayList<UserViewModel> users){
+        UserListViewAdapter adapter;
+
+        if(listView.getAdapter()==null){
+            adapter=new UserListViewAdapter();
+            listView.setAdapter(adapter);
+        }else{
+            adapter= (UserListViewAdapter) listView.getAdapter();
+        }
+
+        adapter.addAll(users);
     }
 }
